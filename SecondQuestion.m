32cbedbae8b2aed1ce821c1c6c3e98dab7401903
Mohamed Moeen <mohamed.moeen166@gmail.com>
2024-12-15 13:49:08 +0200

    ay 7aga

--- SecondQuestion.m
+++ SecondQuestion.m
@@ -23,12 +23,18 @@ xlabel('Frequency (rad/sec)');
 ylabel('|M(f)|');
 title('Fourier Transform');
 
+
+fs =50;
+ts=1/fs;
+t = linspace(-10000, 10000, fs*20000);
+x=(10^-3)*t;
+mt = (sin(x)./x).^2;
 y=2*pi*(10^5)*t;
 rt=mt.*cos(y);
 n=length(rt);
 ft = (ts)*fftshift(fft(rt));
 frequencies = (-n/2:(n/2-1))*(fs/n);
-w=n*frequencies*pi; %de el ghalta el mafrood 2 mesh n, bas shoof eh ghalat tany
+w=2*frequencies*pi;
 figure;
 subplot(2, 1, 1);
 plot(t, rt);
@@ -38,7 +44,7 @@ ylabel('m(t)');
 title('Time Domain');
 subplot(2, 1, 2);
 plot(w, abs(ft));
-xlim([-5e5,5e5]);
+xlim([-1,1]);
 grid on;
 xlabel('Frequency (rad/sec)');
 ylabel('|M(f)|');
